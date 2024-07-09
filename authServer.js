fc7bc4bf44ee808e8c295be106a27c64f608c8da
Yehuda Smilowitz <yehudasmilowitz@gmail.com>
2024-07-08 21:29:53 -0400

    style: format code with Prettier

--- authServer.js
+++ authServer.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -9,72 +9,67 @@ app.use(express.json());
 
 const users = [];
 
-app.get('/users', (req, res) => {
-    // Get all users
-    res.json(users);
-})
-
-app.post('/users', async (req, res) => {
-    try {
-        //const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { name: req.body.name, password: hashedPassword };
-        users.push(user);
-        res.status(201).send();
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send();
-    }
+app.get("/users", (req, res) => {
+  // Get all users
+  res.json(users);
 });
 
-
-app.post('/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name);
-    if (user == null) {
-        return res.status(400).send('Cannot find user');
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const accessToken = generateAccessToken(user);
-            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '24h' });
-            refreshTokens.push(refreshToken);
-            res.json({ accessToken, refreshToken });
-        }
-        else {
-            res.send('Not Allowed');
-        }
-    }
-    catch {
-        res.status(500).send();
-    }
+app.post("/users", async (req, res) => {
+  try {
+    //const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
 
+app.post("/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const accessToken = generateAccessToken(user);
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      refreshTokens.push(refreshToken);
+      res.json({ accessToken, refreshToken });
+    } else {
+      res.send("Not Allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
 //Should be stored in a database or something like redis cache
 let refreshTokens = [];
 
-app.get('/token', (req, res) => {
-    const refreshToken = req.body.token;
-    if (!refreshToken) return res.sendStatus(401);
-    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+app.get("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
 
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        const accessToken = generateAccessToken({ name: user.name });
-        res.json({ accessToken });
-    });
-    
-})
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken });
+  });
+});
 
-app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
-    res.sendStatus(204);
-})
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "60s" });
 }
 
-
-app.listen(4000)
+app.listen(4000);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
@@ -8,32 +8,30 @@ const jwt = require("jsonwebtoken");
 app.use(express.json());
 
 const posts = [
-    {
-        userName: 'johndoe',
-        title: 'Post 1'
-    },
-    {
-        userName: 'Yehuda',
-        title: 'Post 2'
-    }
+  {
+    userName: "johndoe",
+    title: "Post 1",
+  },
+  {
+    userName: "Yehuda",
+    title: "Post 2",
+  },
 ];
 
-app.get('/posts', authenticateToken, (req, res) => {
-    console.log(req.user);
-    res.json(posts.filter(post => post.userName === req.user.name));
-
-})
+app.get("/posts", authenticateToken, (req, res) => {
+  console.log(req.user);
+  res.json(posts.filter((post) => post.userName === req.user.name));
+});
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 
-
-app.listen(3000)
+app.listen(3000);
